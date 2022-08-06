2988cd672a01f5937c87a9acd0a7d3d0932195c7
Adrien Hernandez <adrien.hernandez@rdccaa.com>
2022-08-06 17:06:16 -0400

    modified meta data

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -17,16 +17,15 @@ const Main = ({ children, router }) => {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="description" content="Adrien's homepage" />
         <meta name="author" content="Adrien Hernandez" />
-        <meta name="author" content="craftzdog" />
         <link rel="apple-touch-icon" href="apple-touch-icon.png" />
         <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
         <meta name="twitter:title" content="Adrien Hernandez" />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@craftzdog" />
-        <meta name="twitter:creator" content="@craftzdog" />
+        <meta name="twitter:site" content="@Adrien_Hdz_" />
+        <meta name="twitter:creator" content="@Adrien_Hdz_" />
         <meta name="twitter:image" content="https://www.craftz.dog/card.png" />
         <meta property="og:site_name" content="Adrien Hernandez" />
-        <meta name="og:title" content="Takuya Matsuyama" />
+        <meta name="og:title" content="Adrien Hernandez" />
         <meta property="og:type" content="website" />
         <meta property="og:image" content="https://www.craftz.dog/card.png" />
         <title>Adrien Hernandez - Homepage</title>
