39018feacc4ddae33f23cfbc13cb9d13d1e9480f
AdrienHernandez <adrien.hernandez@rdccaa.com>
2022-08-09 14:59:32 -0400

    3d assets adrien

--- components/voxel-dog.js
+++ components/voxel-dog.js
@@ -53,7 +53,7 @@ const VoxelDog = () => {
 
       // 640 -> 240
       // 8   -> 6
-      const scale = scH * 0.005 + 4.8
+      const scale = scH * 0.00005 + 3.5
       const camera = new THREE.OrthographicCamera(
         -scale,
         scale,
@@ -74,7 +74,7 @@ const VoxelDog = () => {
       controls.target = target
       setControls(controls)
 
-      loadGLTFModel(scene, '/dog.glb', {
+      loadGLTFModel(scene, '/adrien.glb', {
         receiveShadow: false,
         castShadow: false
       }).then(() => {
