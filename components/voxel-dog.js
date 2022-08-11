b4e4a8f2cd4b455064b7b9227ab8643e22435450
AdrienHdz <adrien.hernandez@hec.ca>
2022-08-10 21:18:11 -0400

    modified size of 3d asset

--- components/voxel-dog.js
+++ components/voxel-dog.js
@@ -53,7 +53,7 @@ const VoxelDog = () => {
 
       // 640 -> 240
       // 8   -> 6
-      const scale = scH * 0.00005 + 3
+      const scale = scH * 0.00005 + 3.5
       const camera = new THREE.OrthographicCamera(
         -scale,
         scale,
